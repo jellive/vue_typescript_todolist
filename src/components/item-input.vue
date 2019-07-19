bd7d84e689d76cfd87a216f0703d78bfe6d787dd
PD Jell <jellive7@gmail.com>
2019-07-19 14:20:47 +0900

    init commit

---
+++ src/components/item-input.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="form-group">
+    <input type="text"
+        class="form-control"
+        @keyup.enter="addItem"
+        placeholder="할 일을 입력해주세요.">
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator'
+
+@Component
+export default class ItemInput extends Vue {
+    addItem() {
+        
+    }
+}
+</script>
+
